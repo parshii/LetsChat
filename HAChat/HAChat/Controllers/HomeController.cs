1b950b172d86e6d2869a2c3179265212aec6c0ed
Harshit Rastogi <harshit.rastogi24@live.com>
2014-11-13 14:30:40 +0530

    Lets Chat
    An open source chat platform build on top of signal R with use of jquery
    and html.
    
    Its a first check in with base files and functionality.

---
+++ HAChat/HAChat/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace HAChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public string GetTempPath()
+        {
+            string path = System.Environment.GetEnvironmentVariable("TEMP");
+            if (!path.EndsWith("\\")) path += "\\";
+            return path;
+        }
+   
+    }
+}
